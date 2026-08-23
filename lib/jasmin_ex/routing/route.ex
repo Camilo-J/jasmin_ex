ab53b1eb479869de8f69b8eb48eb0c759da3cf64
Camilo-J <josecamilo1902@outlook.com>
2026-08-22 21:03:43 -0500

    fix(routing): reject malformed connectors and unknown filters
    Validate connector ids through ConnectorRef.new/1 and require every
    filter to be a closed Filter type so Route publication returns a typed
    error instead of crashing Router on snapshot encode.

--- lib/jasmin_ex/routing/route.ex
+++ lib/jasmin_ex/routing/route.ex
@@ -2,6 +2,7 @@ defmodule JasminEx.Routing.Route do
   @moduledoc false
 
   alias JasminEx.Routing.ConnectorRef
+  alias JasminEx.Routing.Filter
 
   @enforce_keys [:kind, :order, :connector, :filters]
   defstruct @enforce_keys
@@ -11,23 +12,36 @@ defmodule JasminEx.Routing.Route do
           kind: kind(),
           order: non_neg_integer(),
           connector: ConnectorRef.t(),
-          filters: [term()]
+          filters: [Filter.t()]
         }
 
-  @spec new(keyword()) :: {:ok, t()} | {:error, :invalid_route}
+  @spec new(keyword()) ::
+          {:ok, t()} | {:error, :invalid_route | :invalid_connector | :invalid_filter}
   def new(attrs) when is_list(attrs) do
     kind = Keyword.get(attrs, :kind)
     order = Keyword.get(attrs, :order)
     connector = Keyword.get(attrs, :connector)
     filters = Keyword.get(attrs, :filters, [])
 
-    if kind in [:static, :default] and is_integer(order) and order >= 0 and
-         match?(%ConnectorRef{}, connector) and is_list(filters) do
-      {:ok, %__MODULE__{kind: kind, order: order, connector: connector, filters: filters}}
-    else
-      {:error, :invalid_route}
+    cond do
+      not (kind in [:static, :default] and is_integer(order) and order >= 0 and is_list(filters)) ->
+        {:error, :invalid_route}
+
+      not valid_connector?(connector) ->
+        {:error, :invalid_connector}
+
+      not Enum.all?(filters, &Filter.valid?/1) ->
+        {:error, :invalid_filter}
+
+      true ->
+        {:ok, %__MODULE__{kind: kind, order: order, connector: connector, filters: filters}}
     end
   end
 
   def new(_attrs), do: {:error, :invalid_route}
+
+  defp valid_connector?(%ConnectorRef{type: :smpp_client, id: id}),
+    do: match?({:ok, _}, ConnectorRef.new(id))
+
+  defp valid_connector?(_connector), do: false
 end
